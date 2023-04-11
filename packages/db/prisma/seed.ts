86caf54873ddab79dbd68f5a662f42736615ca02
akutruff <874049+akutruff@users.noreply.github.com>
2023-04-11 16:34:46 +0000

    add database migration scripts and add a field to the db

---
+++ packages/db/prisma/seed.ts
@@ -0,0 +1,34 @@
+/**
+ * Adds seed data to your db
+ *
+ * @link https://www.prisma.io/docs/guides/database/seed-database
+ */
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const firstPostId = "5c03994c-fc16-47e0-bd02-d218a370a078";
+  await prisma.post.upsert({
+    where: {
+      id: firstPostId,
+    },
+    create: {
+      id: firstPostId,
+      title: "First Post",
+      content: "This is an example post generated from `prisma/seed.ts`",
+      note: "This is a note",
+      secondNote: "This is another note",
+    },
+    update: {},
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
