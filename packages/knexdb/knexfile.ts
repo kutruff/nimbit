bc85f9f79d290595a9c410a1e0d99780921eb96d
akutruff <874049+akutruff@users.noreply.github.com>
2023-04-24 17:45:01 +0000

    add knex experimenting

---
+++ packages/knexdb/knexfile.ts
@@ -0,0 +1,49 @@
+import type { Knex } from "knex";
+
+// Update with your config settings.
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "postgresql",
+    connection: {
+      database: "knexplanner",
+      host: "localhost",
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+    },
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
+
+module.exports = config;
