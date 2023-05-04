ad3ecb5d49184494cd35bdd262c4564889ce6418
akutruff <874049+akutruff@users.noreply.github.com>
2023-05-04 15:33:23 +0000

    remove prisma and other databases.  add schema as own project

--- packages/knexdb/scripts/create-database.sh
+++
@@ -1,7 +0,0 @@
-# $1: path to .env file
-
-if [ -f $1 ]; then
-  export $(echo $(cat $1 | sed 's/#.*//g'| xargs) | envsubst)
-fi
-
-psql -tc "SELECT 1 FROM pg_database WHERE datname = '$PGDBNAME'" | grep -q 1 || psql -c "CREATE DATABASE $PGDBNAME"
